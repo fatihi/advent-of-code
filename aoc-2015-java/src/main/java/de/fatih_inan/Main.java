6b817ae3fecf7f5fa659bbe5ec27cc85255d058d
Fatih Inan <fatihinan@gmail.com>
2025-03-07 11:38:18 +0100

    Rename package

--- aoc-2015-java/src/main/java/de/fatih_inan/Main.java
+++ aoc-2015-java/src/main/java/de/fatih_inan/Main.java
@@ -1,4 +1,4 @@
-package de.fatihi_inan;
+package de.fatih_inan;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
